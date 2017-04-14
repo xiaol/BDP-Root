8287a350ab550d88e5a868c1933f68c6a121a4db
zhangshl <zhangshl@zhangshldeMacBook-Pro.local>
2017-04-14 18:13:49 +0800

    保证LDA和Kmeans各2条

--- webserver/app/services/advertisement/IAdResponseService.scala
+++ webserver/app/services/advertisement/IAdResponseService.scala
@@ -3,7 +3,6 @@ package services.advertisement
 import java.io.{ BufferedWriter, OutputStreamWriter }
 import java.net.{ InetSocketAddress, Socket, SocketAddress }
 import java.util.Date
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 import com.google.inject.ImplementedBy
@@ -12,15 +11,14 @@ import commons.models.news.NewsFeedResponse
 import commons.utils.Sha1Utils
 import dao.userprofiles.UserDeviceDAO
 import io.netty.handler.codec.http.{ DefaultHttpHeaders, HttpHeaders }
-import org.asynchttpclient.{ DefaultAsyncHttpClient, ListenableFuture, Response }
+import org.asynchttpclient._
 import org.springframework.util.Base64Utils
 import play.api.Logger
 import play.api.libs.json.Json
 import utils.AdConfig._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.concurrent.{ Await, Future }
+import scala.concurrent.Future
 
 /**
  * Created by zhangshl on 16/9/9.
@@ -61,6 +59,8 @@ class AdResponseService @Inject() (val userDeviceDAO: UserDeviceDAO) extends IAd
   }
 
   def getAdResponse(body: String, remoteAddress: Option[String]): Future[Option[AdResponse]] = {
+    val cf: DefaultAsyncHttpClientConfig = new DefaultAsyncHttpClientConfig.Builder().setRequestTimeout(300).setConnectTimeout(300).build()
+    val asyncHttpClient: AsyncHttpClient = new DefaultAsyncHttpClient(cf)
     try {
       //替换nginx传过来的真实ip
       val requestbody: String = remoteAddress match {
@@ -78,43 +78,32 @@ class AdResponseService @Inject() (val userDeviceDAO: UserDeviceDAO) extends IAd
       val X_TOKEN: String = Base64Utils.encodeToString(str.getBytes)
 
       //组装header
-      val asyncHttpClient = new DefaultAsyncHttpClient()
       val headers = new DefaultHttpHeaders()
       headers.add(HttpHeaders.Names.CONTENT_TYPE, HttpHeaders.Values.APPLICATION_JSON)
       headers.add("X-TOKEN", X_TOKEN)
 
       //获取广告
-      val result: Future[String] = Future {
-        val f: ListenableFuture[Response] = asyncHttpClient.preparePost(adurl).setBody(requestbody).setHeaders(headers).execute()
-        val response: String = f.get().getResponseBody
-        asyncHttpClient.close()
-        response
-      }
+      val f: ListenableFuture[Response] = asyncHttpClient.preparePost(adurl).setBody(requestbody).setHeaders(headers).execute()
+      val response: String = f.get().getResponseBody
+      asyncHttpClient.close()
+      val adResponse: AdResponse = Json.parse(response).as[AdResponse]
 
-      //广告300毫秒超时
-      try {
-        Await.result(result, Duration(300, TimeUnit.MILLISECONDS))
-        val adResponse = result.map { response =>
-          val adResponse: AdResponse = Json.parse(response).as[AdResponse]
-          if (adResponse.data.nonEmpty && adResponse.data.get.adspace.nonEmpty && adResponse.data.get.adspace.get.head.creative.nonEmpty) {
-            Some(adResponse)
-          } else {
-            None
-          }
+      Future {
+        if (adResponse.data.nonEmpty && adResponse.data.get.adspace.nonEmpty && adResponse.data.get.adspace.get.head.creative.nonEmpty) {
+          Some(adResponse)
+        } else {
+          None
         }
-        adResponse
-      } catch {
-        case ex: Exception => Future { None }
-      } finally {
-        try {
-          if (!asyncHttpClient.isClosed)
-            asyncHttpClient.close()
-        } catch { case ex: Exception => Logger.error(s"Within AdResponseService.getAdResponse(): ${ex.getMessage}") }
       }
-
     } catch {
       case ex: Exception =>
         Logger.error(s"Within AdResponseService.getAdResponse(): ${ex.getMessage}")
+        try {
+          if (!asyncHttpClient.isClosed)
+            asyncHttpClient.close()
+        } catch {
+          case ex: Exception => Logger.error(s"Within AdResponseService.getAdResponse(): Close asyncHttpClient Failure, ${ex.getMessage}")
+        }
         Future { None }
     }
   }
