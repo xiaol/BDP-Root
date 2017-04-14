fe1cf15eed5550a7d17d54f143af19f2da57bfe6
zhangshl <zhangshl@zhangshldeMacBook-Pro.local>
2017-04-14 11:45:43 +0800

    update advertisement bug

--- webserver/app/services/advertisement/IAdResponseService.scala
+++ webserver/app/services/advertisement/IAdResponseService.scala
@@ -3,6 +3,7 @@ package services.advertisement
 import java.io.{ BufferedWriter, OutputStreamWriter }
 import java.net.{ InetSocketAddress, Socket, SocketAddress }
 import java.util.Date
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 import com.google.inject.ImplementedBy
@@ -18,7 +19,8 @@ import play.api.libs.json.Json
 import utils.AdConfig._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future }
 
 /**
  * Created by zhangshl on 16/9/9.
@@ -31,49 +33,34 @@ trait IAdResponseService {
 
 class AdResponseService @Inject() (val userDeviceDAO: UserDeviceDAO) extends IAdResponseService {
 
-  def getAdNewsFeedResponse(body: String, remoteAddress: Option[String]): Future[Seq[NewsFeedResponse]] = Future {
+  def getAdNewsFeedResponse(body: String, remoteAddress: Option[String]): Future[Seq[NewsFeedResponse]] = {
     try {
-      //替换nginx传过来的真实ip
-      val requestbody: String = remoteAddress match {
-        case Some(ip) =>
-          val request: AdRequest = Json.parse(body).as[AdRequest]
-          val adRequest = request.copy(device = request.device.copy(ip = Some(ip)))
-          Json.toJson(adRequest).toString()
-        case _ => body
-      }
+      val result: Future[Option[AdResponse]] = getAdResponse(body, remoteAddress)
 
-      val nowtime: Long = new Date().getTime / 1000
-      val sign: String = Sha1Utils.encodeSha1(adappkey + "|" + nowtime)
-      val str = adappid + "|" + nowtime + "|" + sign
-      val X_TOKEN: String = Base64Utils.encodeToString(str.getBytes)
-
-      val asyncHttpClient = new DefaultAsyncHttpClient()
-      val headers = new DefaultHttpHeaders()
-      headers.add(HttpHeaders.Names.CONTENT_TYPE, HttpHeaders.Values.APPLICATION_JSON)
-      headers.add("X-TOKEN", X_TOKEN)
-      //.executeRequest()可以设置超时时间
-      val f: ListenableFuture[Response] = asyncHttpClient.preparePost(adurl).setBody(requestbody).setHeaders(headers).execute()
-      val response: String = f.get().getResponseBody
-      asyncHttpClient.close()
-      val adResponse: AdResponse = Json.parse(response).as[AdResponse]
-      if (adResponse.data.nonEmpty && adResponse.data.get.adspace.nonEmpty && adResponse.data.get.adspace.get.head.creative.nonEmpty) {
-        val list: List[Creative] = adResponse.data.get.adspace.get.head.creative.get
-        val seq: Seq[NewsFeedResponse] = list.map {
-          case creative: Creative =>
-            NewsFeedResponse.from(creative).copy(adresponse = Some(adResponse))
-        }.toSeq
-        seq
-      } else {
-        Seq[NewsFeedResponse]()
+      result.map { response =>
+        response match {
+          case Some(adResponse: AdResponse) =>
+            if (adResponse.data.nonEmpty && adResponse.data.get.adspace.nonEmpty && adResponse.data.get.adspace.get.head.creative.nonEmpty) {
+              val list: List[Creative] = adResponse.data.get.adspace.get.head.creative.get
+              val seq: Seq[NewsFeedResponse] = list.map {
+                case creative: Creative =>
+                  NewsFeedResponse.from(creative).copy(adresponse = Some(adResponse))
+              }
+              seq
+            } else {
+              Seq[NewsFeedResponse]()
+            }
+          case _ => Seq[NewsFeedResponse]()
+        }
       }
     } catch {
       case ex: Exception =>
         Logger.error(s"Within AdResponseService.getAdNewsFeedResponse(): ${ex.getMessage}")
-        Seq[NewsFeedResponse]()
+        Future { Seq[NewsFeedResponse]() }
     }
   }
 
-  def getAdResponse(body: String, remoteAddress: Option[String]): Future[Option[AdResponse]] = Future {
+  def getAdResponse(body: String, remoteAddress: Option[String]): Future[Option[AdResponse]] = {
     try {
       //替换nginx传过来的真实ip
       val requestbody: String = remoteAddress match {
@@ -84,29 +71,51 @@ class AdResponseService @Inject() (val userDeviceDAO: UserDeviceDAO) extends IAd
         case _ => body
       }
 
+      //组装TOKEN
       val nowtime: Long = new Date().getTime / 1000
       val sign: String = Sha1Utils.encodeSha1(adappkey + "|" + nowtime)
       val str = adappid + "|" + nowtime + "|" + sign
       val X_TOKEN: String = Base64Utils.encodeToString(str.getBytes)
 
+      //组装header
       val asyncHttpClient = new DefaultAsyncHttpClient()
       val headers = new DefaultHttpHeaders()
       headers.add(HttpHeaders.Names.CONTENT_TYPE, HttpHeaders.Values.APPLICATION_JSON)
       headers.add("X-TOKEN", X_TOKEN)
-      //.executeRequest()可以设置超时时间
-      val f: ListenableFuture[Response] = asyncHttpClient.preparePost(adurl).setBody(requestbody).setHeaders(headers).execute()
-      val response: String = f.get().getResponseBody
-      asyncHttpClient.close()
-      val adResponse: AdResponse = Json.parse(response).as[AdResponse]
-      if (adResponse.data.nonEmpty && adResponse.data.get.adspace.nonEmpty && adResponse.data.get.adspace.get.head.creative.nonEmpty) {
-        Some(adResponse)
-      } else {
-        None
+
+      //获取广告
+      val result: Future[String] = Future {
+        val f: ListenableFuture[Response] = asyncHttpClient.preparePost(adurl).setBody(requestbody).setHeaders(headers).execute()
+        val response: String = f.get().getResponseBody
+        asyncHttpClient.close()
+        response
       }
+
+      //广告300毫秒超时
+      try {
+        Await.result(result, Duration(300, TimeUnit.MILLISECONDS))
+        val adResponse = result.map { response =>
+          val adResponse: AdResponse = Json.parse(response).as[AdResponse]
+          if (adResponse.data.nonEmpty && adResponse.data.get.adspace.nonEmpty && adResponse.data.get.adspace.get.head.creative.nonEmpty) {
+            Some(adResponse)
+          } else {
+            None
+          }
+        }
+        adResponse
+      } catch {
+        case ex: Exception => Future { None }
+      } finally {
+        try {
+          if (!asyncHttpClient.isClosed)
+            asyncHttpClient.close()
+        } catch { case ex: Exception => Logger.error(s"Within AdResponseService.getAdResponse(): ${ex.getMessage}") }
+      }
+
     } catch {
       case ex: Exception =>
         Logger.error(s"Within AdResponseService.getAdResponse(): ${ex.getMessage}")
-        None
+        Future { None }
     }
   }
 
@@ -125,9 +134,6 @@ class AdResponseService @Inject() (val userDeviceDAO: UserDeviceDAO) extends IAd
       bufferedWriter.flush()
       streamWriter.flush()
       socket.close()
-      //    val asyncHttpClient = new DefaultAsyncHttpClient()
-      //    asyncHttpClient.prepareGet(deleteAdUrl_v + "?nid=" + nid).execute().get()
-      //    asyncHttpClient.close()
     }
   }
 }
